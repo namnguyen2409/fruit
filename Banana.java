17172e434470904aef5b0d1c4039796652bd8470
Nam Nguyen <namnguyen@github.com>
2017-10-06 15:06:53 +0700

    modifying Banana: change NAME and addding garbage

--- Banana.java
+++ Banana.java
@@ -1,12 +1,16 @@
 package edu.eiu.fruit;
 
 public class Banana implements IFruit {
-    private static final String  NAME = "Banana";
+    private static final String  NAME = "Banana Veg";
     private static final int ROLL_MULTIPLIER = 0;
     public String getName() {
         return NAME;
     }
 
     public int getRollMultiplier() { return ROLL_MULTIPLIER;}
+    public  void garbage(){
+        int x = 1;
+        int y = 2;
+    }
 }
 
